2cd9d6351373ca684d04d4b2843213fe2193d04f
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-04-20 17:06:04 +0530

    Update age.js to handle async auth
    Ensure that rank is stored before tools.spliceRank is called

--- commands/global/age.js
+++ commands/global/age.js
@@ -4,12 +4,13 @@ module.exports = {
     permissions: 'none',
     commandFunction: function (Bot, room, time, by, args, client) {
         if (!args[0]) return Bot.say(room, unxa);
+        let rank = tools.hasPermission(by, 'gamma');
         require('request')('http://pokemonshowdown.com/users/' + toId(args.join(' ')) + '.json', (err, response, body) => {
             if (err) throw err;
             let userObj = JSON.parse(body);
             if (!userObj.registertime) return Bot.say(room, toId(args.join(' ')) + ' is not registered.');
             let dtime = Date.now() - 1000*userObj.registertime;
-            if (tools.hasPermission(by, 'gamma')) Bot.say(room, userObj.username + ' is ' + tools.humanTime(dtime) + ' old.');
+            if (rank) Bot.say(room, userObj.username + ' is ' + tools.humanTime(dtime) + ' old.');
             else Bot.pm(by, userObj.username + ' is ' + tools.humanTime(dtime) + ' old.');
         });
     }
