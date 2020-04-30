a0efff7a5ad22b30de12c788a1a81f528688ed6b
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-04-30 16:42:15 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-// Really updated version of Ecuacion's connection.
+// Updated version of Ecuacion's connection.
 
 var util = require('util');
 var https = require('https');
@@ -35,31 +35,74 @@ class Client extends EventEmitter {
 			autoJoin: [],
 			showErrors: true,
 			debug: false,
-		};
+		}
 		if (typeof opts === 'object') {
 			for (var i in opts) 
 				this.opts[i] = opts[i];
 		}
 		this.actionUrl = url.parse(this.opts.loginServer);
+
+		this.app = opts.app;
+		this.keys = {};
+		this.pageData = {};
 	
 		this.rooms = {};
+
+		this.jps = {};
+		this.jpcool = {};
+		fs.readdir('./data/JPS', (err, files) => {
+			if (err) return;
+			files.forEach(room => {
+				fs.readFile(`./data/JPS/${room}`, 'utf8', (e, file) => {
+					if (e) return console.log(e);
+					this.jps[room.substr(0, room.length - 5)] = JSON.parse(file);
+				});
+			});
+		});
+
+		this.auth = Object.assign({}, config.auth);
+		
+		this.userCallbacks = {};
+		this.userCallbackData = {};
+
+		fs.readFile('./data/DATA/roomauth.json', 'utf8', (err, file) => {
+			if (err) return Bot.log(err);
+			let obj = JSON.parse(file);
+			this.baseAuth = {};
+			Object.keys(obj).forEach(room => {
+				this.baseAuth[room] = {};
+				obj[room].gamma.forEach(u => this.baseAuth[room][u] = 3);
+				obj[room].beta.forEach(u => this.baseAuth[room][u] = 4);
+				obj[room].alpha.forEach(u => this.baseAuth[room][u] = 5);
+			});
+		});
 	
 		this.connection = null;
 		this.status = {
 			connected: false,
 			nickName: null
-		};
+		}
 		this.challstr = {
 			id: 0,
 			str: ''
-		};
+		}
 		this.events = {};
 		this.debug = function (str) {
 			if (this.opts.debug) console.log("DEBUG: " + str);
-		};
+		}
 		this.error = function (str) {
 			if (this.opts.showErrors) console.log("ERROR: " + str);
-		};
+		}
+		this.getRooms = function (user) {
+			user = toId(user);
+			let out;
+			try {
+				return out = Object.keys(this.rooms).filter(room => this.rooms[room].users.includes(user));
+			} catch (e) {
+				if (e) return out = [];
+			}
+			return out;
+		}
 	}
 }
 	
@@ -70,11 +113,34 @@ Client.prototype.init = function () {
 	if (this.opts.connectionTimeout) {
 		this.startConnectionTimeOut();
 	}
-};
+}
+
+Client.prototype.serve = function (user, html) {
+	if (!config.site) return null;
+	let id = toId(user);
+	if (!this.keys[id]) this.keys[id] = 7000 + Math.floor(Math.random() * 93000);
+	this.pageData[id] = html;
+	this.app.get(`/${id}-${this.keys[id]}`, (req, res) => {
+		return res.send(`<head>\n\t<title>${user.substr(1)}</title>\n\t<link rel="icon" href="https://cdn.glitch.com/26f69991-dcb5-4447-bd9e-4b58070b20bc%2Fpngkey.com-gengar-png-9726433.png">\n</head>\n<body>\t${this.pageData[id]}</body>`);
+	});
+	return this.pm(user, `http://${process.env.PROJECT_DOMAIN}.glitch.me/${id}-${Bot.keys[id]}`);
+}
+
+Client.prototype.sendHTML = function (user, html) {
+	let cRooms = this.getRooms(user);
+	for (let room of cRooms) {
+		if (this.rooms[room] && ['*', '#', '&', '~', '★'].includes(this.rooms[room].rank)) return this.say(room, `/pminfobox ${user}, ${html.replace(/\n/g, '')}`);
+	}
+	if (!this.keys) return undefined;
+	let id = toId(user);
+	if (!this.keys[id]) this.keys[id] = 7000 + Math.floor(Math.random() * 93000);
+	if (config.site) return this.serve(user, html);
+	return null;
+}
 
 
 /********************************
-*	Connection
+* Connection
 *********************************/
 
 Client.prototype.connect = function (retry) {
@@ -145,19 +211,19 @@ Client.prototype.connect = function (retry) {
 	var conStr = 'ws://' + self.opts.server + ':' + self.opts.port + '/showdown/' + id + '/' + str + '/websocket';
 	self.debug('connecting to ' + conStr + ' - secondary protocols: ' + util.inspect(self.opts.secprotocols));
 	webSocket.connect(conStr, self.opts.secprotocols);
-};
+}
 
 Client.prototype.disconnect = function () {
 	this.closed = true;
 	if (this.connection) this.connection.close();
-};
+}
 
 Client.prototype.softDisconnect = function () {
 	if (this.connection) this.connection.close();
-};
+}
 
 /********************************
-*	Login
+* Login
 *********************************/
 
 Client.prototype.rename = function (nick, pass) {
@@ -166,7 +232,7 @@ Client.prototype.rename = function (nick, pass) {
 		port: this.actionUrl.port,
 		path: this.actionUrl.pathname,
 		agent: false
-	};
+	}
 	if (!pass) {
 		requestOptions.method = 'GET';
 		requestOptions.path += '?act=getassertion&userid=' + toId(nick) + '&challengekeyid=' + this.challstr.id + '&challenge=' + this.challstr.str;
@@ -177,7 +243,7 @@ Client.prototype.rename = function (nick, pass) {
 		requestOptions.headers = {
 			'Content-Type': 'application/x-www-form-urlencoded',
 			'Content-Length': data.length
-		};
+		}
 		this.debug("Sending log in request to: " + requestOptions.path + " | Data -> " + data);
 	}
 	var req = https.request(requestOptions, function(res) {
@@ -247,10 +313,10 @@ Client.prototype.rename = function (nick, pass) {
 		req.write(data);
 	}
 	req.end();
-};
+}
 
 /********************************
-*	Sending data
+* Sending data
 *********************************/
 
 Client.prototype.send = function (data, delay) {
@@ -273,7 +339,7 @@ Client.prototype.send = function (data, delay) {
 					self.events['send'](toSend);
 				}
 				connection.send(toSend);
-			};
+			}
 			var spacer = setInterval(nextToSend, delay);
 			nextToSend();
 		} else {
@@ -289,7 +355,7 @@ Client.prototype.send = function (data, delay) {
 			self.events['sendfailure'](-1);
 		}
 	}
-};
+}
 
 Client.prototype.sendRoom = function (room, data, delay) {
 	if (!(data instanceof Array)) {
@@ -299,18 +365,18 @@ Client.prototype.sendRoom = function (room, data, delay) {
 		data[i] = room + '|' + data[i];
 	}
 	this.send(data, delay);
-};
+}
 
 Client.prototype.say = function (room, msg) {
 	if (room.charAt(0) === ',') {
 		return this.pm(room.substr(1), msg);
 	}
 	this.send(room + '|' + msg);
-};
+}
 
 Client.prototype.pm = function (user, msg) {
 	this.send('|/pm ' + user + ',' + msg);
-};
+}
 
 Client.prototype.joinRooms = function (rooms) {
 	var cmds = [];
@@ -320,15 +386,15 @@ Client.prototype.joinRooms = function (rooms) {
 		cmds.push('|/join ' + room);
 	}
 	if (cmds.length) this.send(cmds);
-};
+}
 
 Client.prototype.setAvatar = function (avatar) {
 	this.send('|/avatar ' + avatar);
-};
+}
 
 Client.prototype.setStatus = function (status) {
 	this.send('botdevelopment|/status ' + status);
-};
+}
 
 Client.prototype.leaveRooms = function (rooms) {
 	var cmds = [];
@@ -338,7 +404,7 @@ Client.prototype.leaveRooms = function (rooms) {
 		cmds.push('|/leave ' + room);
 	}
 	if (cmds.length) this.send(cmds);
-};
+}
 
 Client.prototype.log = function (text) {
 	fs.appendFile('./logs.txt', `\n${text}`, function (e) {
@@ -348,7 +414,7 @@ Client.prototype.log = function (text) {
 }
 
 /********************************
-*	Data reception
+* Data reception
 *********************************/
 
 Client.prototype.receive = function (message) {
@@ -367,7 +433,7 @@ Client.prototype.receive = function (message) {
 			}
 			break;
 	}
-};
+}
 
 Client.prototype.receiveMsg = function (message) {
 	if (!message) return;
@@ -391,146 +457,182 @@ Client.prototype.receiveMsg = function (message) {
 	} else {
 		this.receiveLine('lobby', message);
 	}
-};
+}
 
 Client.prototype.receiveLine = function (room, message, isIntro) {
 	var larg = message.substr(1).split('|');
 	Bot.emit('line', room, message, isIntro, larg);
-  switch (larg[0]) {
-    case 'formats':
-      break;
-    case 'challstr':
-      this.challstr = {
-        id: larg[1],
-        str: larg[2]
-      }
-      if (this.opts.nickName) this.rename(this.opts.nickName, this.opts.pass);
-      break;
-    case 'updateuser':
-      this.status.nickName = larg[1].substr(1);
-      console.log('Connected to Pokémon Showdown.');
-      if (!this.opts.status) this.setStatus('say ' + this.status.nickName + '? for help.');
-      if (parseInt(larg[2])) this.joinRooms(config.autoJoin);
-      if (this.opts.status) this.setStatus(this.opts.status);
-      if (this.opts.avatar) this.setAvatar(this.opts.avatar);
-      break;
-    case 'c': {
-      let by = larg[1];
-      let timeOff = Date.now();
-      larg.splice(0, 2);
-      if (isIntro) Bot.emit('intro', room, timeOff, by, larg.join('|'));
-      else if (by.substr(1) === this.status.nickName) Bot.emit('chatsuccess', room, timeOff, by, larg.join('|'));
-      else Bot.emit('chat', room, timeOff, by, larg.join('|'));
-      break;
-    }
-    case 'c:': {
-      let by = larg[2];
-      let timeOff = parseInt(larg[1]) * 1000;
-      larg.splice(0, 3);
-      if (isIntro) Bot.emit('intro', room, timeOff, by, larg.join('|'));
-      else if (by.substr(1) === this.status.nickName) Bot.emit('chatsuccess', room, timeOff, by, larg.join('|'));
-      else Bot.emit('chat', room, timeOff, by, larg.join('|'));
-      break;
-    }
-    case 'init':
-      if (!this.rooms[room]) this.rooms[room] = {tourActive: false, rank: false};
-      setTimeout(()=>Bot.say(room, '.'), 1000);
-      break;
-    case 'deinit':
-      if (this.rooms[room]) delete this.rooms[room];
-      break;
-    case 'popup':
-      Bot.emit('popup', larg.join('|').substr(6));
-      break;
-    case 'error':
-      larg.splice(0, 1);
-      Bot.emit('chaterror', room, larg.join('|'), isIntro);
-      break;
-    case 'popup':
-      larg.splice(0, 1);
-      Bot.emit('popup', larg.join('|'));
-      break;
-    case 'tour':
-      Bot.emit('tour', larg.join('|'));
-      break;
-    case 'pm': {
-      let by = larg[1];
-      let dest = larg[2];
-      larg.splice(0, 3);
-      let pmcontent = larg.join('|');
-      if (pmcontent.startsWith('/error ')) break;
-      else if (by.substr(1) === this.status.nickName) Bot.emit('pmsuccess', dest, pmcontent);
-      else Bot.emit('pm', by, pmcontent);
-      break;
-    }
-    case 'queryresponse':
-      if (larg[1] === 'userdetails') {
-        larg.splce(0, 2);
-        Bot.emit('userdetails', larg.join('|'));
-      }
-      else {
-        larg.splice(0, 1);
-        Bot.emit('queryresponse', larg.join('|'));
-      }
-      break;
-    case 'n': case 'N':
-      var by = toId(larg[1].substr(1).split('@')[0]);
-      var old = toId(larg[2]);
-      let rank = tools.rankLevel(old);
-      switch (rank) {
-        case 10:
-          if (admin.includes(old) && !(by == old) && !adminalts.includes(by) && !admin.includes(by)) adminalts.push(by);
-          else if (adminalts.includes(old)) {
-            adminalts.splice(adminalts.indexOf(old), 1);
-            if (!admin.includes(by)) adminalts.push(by);
-          }
-          break;
-        case 9:
-          if (coder.includes(old) && !(by == old) && !coderalts.includes(by) && !coder.includes(by)) coderalts.push(by);
-          else if (coderalts.includes(old)) {
-            coderalts.splice(coderalts.indexOf(old), 1);
-            if (!coder.includes(by)) coderalts.push(by);
-          }
-          break;
-        case 5:
-          if (alpha.includes(old) && !(by == old) && !alphaalts.includes(by) && !alpha.includes(by)) alphaalts.push(by);
-          else if (alphaalts.includes(old)) {
-            alphaalts.splice(alphaalts.indexOf(old), 1);
-            if (!alpha.includes(by)) alphaalts.push(by);
-          }
-          break;
-        case 4:
-          if (beta.includes(old) && !(by == old) && !betaalts.includes(by) && !beta.includes(by)) betaalts.push(by);
-          else if (betaalts.includes(old)) {
-            betaalts.splice(betaalts.indexOf(old), 1);
-            if (!beta.includes(by)) betaalts.push(by);
-          }
-          break;
-        case 3:
-          if (gamma.includes(old) && !(by == old) && !gammaalts.includes(by) && !gamma.includes(by)) gammaalts.push(by);
-          else if (gammaalts.includes(old)) {
-            gammaalts.splice(gammaalts.indexOf(old), 1);
-            if (!gamma.includes(by)) gammaalts.push(by);
-          }
-          break;
-        case 1:
-          if (locked.includes(old) && !(by == old) && !lockedalts.includes(by) && !locked.includes(by)) lockedalts.push(by);
-          else if (lockedalts.includes(old)) {
-            lockedalts.splice(lockedalts.indexOf(old), 1);
-            if (!locked.includes(by)) lockedalts.push(by);
-          }
-          break;
-        default:
-          break;
-      }
-    default:
-      Bot.emit('other', larg.join('|'));
-      break;
+	switch (larg[0]) {
+		case 'formats':
+			break;
+		case 'challstr':
+			this.challstr = {
+				id: larg[1],
+				str: larg[2]
+			}
+			if (this.opts.nickName) this.rename(this.opts.nickName, this.opts.pass);
+			break;
+		case 'updateuser':
+			this.status.nickName = larg[1].substr(1);
+			console.log('Connected to Pokémon Showdown.');
+			if (!this.opts.status) this.setStatus('say ' + this.status.nickName + '? for help.');
+			if (parseInt(larg[2])) this.joinRooms(config.autoJoin);
+			if (this.opts.status) this.setStatus(this.opts.status);
+			if (this.opts.avatar) this.setAvatar(this.opts.avatar);
+			break;
+		case 'c': {
+			let by = larg[1];
+			let timeOff = Date.now();
+			larg.splice(0, 2);
+			if (isIntro) Bot.emit('intro', room, timeOff, by, larg.join('|'));
+			else if (by.substr(1) === this.status.nickName) Bot.emit('chatsuccess', room, timeOff, by, larg.join('|'));
+			else Bot.emit('chat', room, timeOff, by, larg.join('|'));
+			break;
 		}
-};
+		case 'c:': {
+			let by = larg[2];
+			let timeOff = parseInt(larg[1]) * 1000;
+			larg.splice(0, 3);
+			if (isIntro) Bot.emit('intro', room, timeOff, by, larg.join('|'));
+			else if (by.substr(1) === this.status.nickName) Bot.emit('chatsuccess', room, timeOff, by, larg.join('|'));
+			else Bot.emit('chat', room, timeOff, by, larg.join('|'));
+			break;
+		}
+		case 'init':
+			if (!this.rooms[room]) {
+				this.rooms[room] = {rank: false, users: []};
+				if (this.baseAuth[room]) this.rooms[room].auth = Object.assign({}, this.baseAuth[room]);
+				tools.loadShops([room]);
+				this.jpcool[room] = {};
+			}
+			setTimeout(()=>Bot.say(room, '.'), 1000);
+			break;
+		case 'users': {
+			let args = larg[1].split(',');
+			let amt = parseInt(args.shift());
+			if (args.length !== amt) console.log('User initialization error: length did not match.', room);
+			args.forEach(user => Bot.rooms[room].users.push(toId(user)));
+			break;
+		}
+		case 'title':
+			if (this.rooms[room]) this.rooms[room].title = larg[1];
+			break;
+		case 'deinit':
+			if (this.rooms[room]) delete this.rooms[room];
+			break;
+		case 'popup':
+			Bot.emit('popup', larg.join('|').substr(6));
+			break;
+		case 'error':
+			larg.splice(0, 1);
+			Bot.emit('chaterror', room, larg.join('|'), isIntro);
+			break;
+		case 'popup':
+			larg.splice(0, 1);
+			Bot.emit('popup', larg.join('|'));
+			break;
+		case 'tour': case 'tournament':
+			if (!isIntro) Bot.emit('tour', room, larg.slice(1, larg.length));
+			break;
+		case 'pm': {
+			let by = larg[1];
+			let dest = larg[2];
+			larg.splice(0, 3);
+			let pmcontent = larg.join('|');
+			if (pmcontent.startsWith('/error ')) break;
+			else if (by.substr(1) === this.status.nickName) Bot.emit('pmsuccess', dest, pmcontent);
+			else Bot.emit('pm', by, pmcontent);
+			break;
+		}
+		case 'queryresponse': {
+			if (larg[1] === 'userdetails') {
+				larg.splce(0, 2);
+				Bot.emit('userdetails', larg.join('|'));
+			}
+			else {
+				larg.splice(0, 1);
+				Bot.emit('queryresponse', larg.join('|'));
+			}
+			break;
+		}
+		case 'j': case 'J': 
+			this.rooms[room].users.push(toId(larg[1]));
+			Bot.emit('join', toId(larg[1]), room, Date.now());
+			break;
+		case 'n': case 'N': 
+			var by = toId(larg[1]);
+			var old = toId(larg[2]);
+			while (this.rooms[room].users.includes(old)) this.rooms[room].users.splice(this.rooms[room].users.indexOf(old), 1);
+			this.rooms[room].users.push(by);
+			let rank = tools.rankLevel(old);
+			if (old === toId(this.status.nickName)) {
+				this.rooms[room].rank = larg[1].charAt(0);
+			}
+			switch (rank) {
+				case 10:
+					if (this.auth.admin.includes(old) && !(by == old) && !this.auth.adminalts.includes(by) && !this.auth.admin.includes(by)) this.auth.adminalts.push(by);
+					else if (this.auth.adminalts.includes(old)) {
+						this.auth.adminalts.splice(this.auth.adminalts.indexOf(old), 1);
+						if (!this.auth.admin.includes(by)) this.auth.adminalts.push(by);
+					}
+					break;
+				case 9:
+					if (this.auth.coder.includes(old) && !(by == old) && !this.auth.coderalts.includes(by) && !this.auth.coder.includes(by)) this.auth.coderalts.push(by);
+					else if (this.auth.coderalts.includes(old)) {
+						this.auth.coderalts.splice(this.auth.coderalts.indexOf(old), 1);
+						if (!this.auth.coder.includes(by)) this.auth.coderalts.push(by);
+					}
+					break;
+				case 5:
+					if (this.auth.alpha.includes(old) && !(by == old) && !this.auth.alphaalts.includes(by) && !this.auth.alpha.includes(by)) this.auth.alphaalts.push(by);
+					else if (this.auth.alphaalts.includes(old)) {
+						this.auth.alphaalts.splice(this.auth.alphaalts.indexOf(old), 1);
+						if (!this.auth.alpha.includes(by)) this.auth.alphaalts.push(by);
+					}
+					break;
+				case 4:
+					if (this.auth.beta.includes(old) && !(by == old) && !this.auth.betaalts.includes(by) && !this.auth.beta.includes(by)) this.auth.betaalts.push(by);
+					else if (this.auth.betaalts.includes(old)) {
+						this.auth.betaalts.splice(this.auth.betaalts.indexOf(old), 1);
+						if (!this.auth.beta.includes(by)) this.auth.betaalts.push(by);
+					}
+					break;
+				case 3:
+					if (room.startsWith('groupchat-')) return;
+					if (this.auth.gamma.includes(old) && !(by == old) && !this.auth.gammaalts.includes(by) && !this.auth.gamma.includes(by)) this.auth.gammaalts.push(by);
+					else if (this.auth.gammaalts.includes(old)) {
+						this.auth.gammaalts.splice(this.auth.gammaalts.indexOf(old), 1);
+						if (!this.auth.gamma.includes(by)) this.auth.gammaalts.push(by);
+					}
+					break;
+				case 1:
+					if (this.auth.locked.includes(old) && !(by == old) && !this.auth.lockedalts.includes(by) && !this.auth.locked.includes(by)) this.auth.lockedalts.push(by);
+					else if (this.auth.lockedalts.includes(old)) {
+						this.auth.lockedalts.splice(this.auth.lockedalts.indexOf(old), 1);
+						if (!this.auth.locked.includes(by)) this.auth.lockedalts.push(by);
+					}
+					break;
+				default:
+					break;
+			}
+			break;
+		case 'l': case 'L':
+			var user = toId(larg[1]);
+			while (this.rooms[room].users.includes(user)) this.rooms[room].users.splice(this.rooms[room].users.indexOf(user), 1);
+			break;
+    case 'raw': {
+      Bot.emit('raw', room, larg.join('|'), isIntro);
+      break;
+    }
+		default: {
+			Bot.emit('other', room, larg.join('|'));
+			break;
+		}
+	}
+}
 
-/********************************
-*	Connection Timer
+/*********************************
+*	   Connection Timer          *
 *********************************/
 
 Client.prototype.startConnectionTimeOut = function () {
@@ -545,10 +647,10 @@ Client.prototype.startConnectionTimeOut = function () {
 			}
 		}
 	}, self.opts.connectionTimeout);
-};
+}
 
 Client.prototype.stopConnectionTimeOut = function () {
 	if (this.connectionTimeOutInterval) clearInterval(this.connectionTimeOutInterval);
-};
+}
 
 module.exports = Client;
