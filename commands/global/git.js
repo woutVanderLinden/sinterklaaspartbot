db4456522b2aadb2245be7866dd742161c841e43
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-05-02 17:25:34 +0530

    Make git broadcastable

--- commands/global/git.js
+++ commands/global/git.js
@@ -1,8 +1,9 @@
 module.exports = {
-    cooldown: 10000,
+    cooldown: 10,
     help: `Links the GitHub for PartBot's code repository.`,
     permissions: 'none',
     commandFunction: function (Bot, room, time, by, args, client) {
-        Bot.say(room, `/w ${by}, https://github.com/PartMan7/PartBot`);
+        if (tools.hasPermission(by, 'gamma', room)) Bot.say(room, 'https://github.com/PartMan7/PartBot');
+        else Bot.pm(by, `https://github.com/PartMan7/PartBot`);
     }
 }
