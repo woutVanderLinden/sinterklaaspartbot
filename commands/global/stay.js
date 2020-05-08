213856018ac42752afaf23f9931b1cdc1af79b57
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-05-08 14:17:30 +0530

    Fix stay permissions

--- commands/global/stay.js
+++ commands/global/stay.js
@@ -4,7 +4,7 @@ module.exports = {
   permissions: 'none',
   commandFunction: function (Bot, room, time, by, args, client) {
     if (!Bot.rooms[room].blackjack) return Bot.pm(by, 'No game of Blackjhack is currently active...');
-    if (!Bot.rooms[room].blackjack.turn == toId(by)) return Bot.pm(by, `It's not your turn.`);
+    if (Bot.rooms[room].blackjack.turn !== toId(by)) return Bot.pm(by, `It's not your turn.`);
     let user = Bot.rooms[room].blackjack.players[toId(by)];
     if (tools.sumBJ(user.cards) == 21) {
       if (user.cards.length == 2) user.nbj = true;
