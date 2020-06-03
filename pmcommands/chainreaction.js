d57f366bf204da97bea2a2d1d570e9d07385b252
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-06-03 14:43:48 +0530

    Create chainreaction.js

---
+++ pmcommands/chainreaction.js
@@ -0,0 +1,12 @@
+module.exports = {
+	help: `Just use the buttons.`,
+	permissions: 'none',
+	commandFunction: function (Bot, by, args, client) {
+		let crCom = require('../commands/global/chainreaction.js').commandFunction;
+		let room = args.shift();
+		if (!room) return Bot.pm(by, unxa);
+		room = room.replace(/[^0-9a-z-]/g,'');
+		if (!Bot.rooms[room]) return Bot.pm(by, "Invalid room.");
+		return crCom(Bot, room, Date.now(), by, args, client, true);
+	}
+}
