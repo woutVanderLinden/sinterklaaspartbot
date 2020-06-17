2470f242d0815ec150b5a162b375813c07d313e1
arifin6686 <43263817+arifin6686@users.noreply.github.com>
2020-06-17 13:21:55 +0530

    Fix typo in client.js

--- client.js
+++ client.js
@@ -546,7 +546,7 @@ Client.prototype.receiveLine = function (room, message, isIntro) {
 		}
 		case 'queryresponse': {
 			if (larg[1] === 'userdetails') {
-				larg.splce(0, 2);
+				larg.splice(0, 2);
 				Bot.emit('userdetails', larg.join('|'));
 			}
 			else {
