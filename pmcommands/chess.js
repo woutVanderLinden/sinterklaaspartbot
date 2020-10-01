46bd3724d3f3b59d3be79f74514263e8136f7935
Chirag26py <56684699+Chirag26py@users.noreply.github.com>
2020-10-01 12:41:37 +0530

    Make chess noDisplay

--- pmcommands/chess.js
+++ pmcommands/chess.js
@@ -1,5 +1,6 @@
 module.exports = {
 	help: `The Chess module. Use \`\`play (room) (position)-(position)\`\` to play.`,
+	noDisplay: true,
 	permissions: 'none',
 	commandFunction: function (Bot, by, args, client) {
 		let chessCom = require('../commands/global/chess.js').commandFunction;
@@ -7,6 +8,6 @@ module.exports = {
 		if (!room) return Bot.pm(by, unxa);
 		room = room.replace(/[^0-9a-z-]/g,'');
 		if (!Bot.rooms[room]) return Bot.pm(by, "Invalid room.");
-		return chessCom(Bot, room, Date.now(), by, args, client);
+		return chessCom(Bot, room, Date.now(), by, args, client, true);
 	}
 }
