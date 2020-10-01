23df18414c4435a819330aa9b1a6431d8bfa712c
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-10-01 09:59:55 +0530

    Change chat to chat handler

--- chat.js
+++ chat.js
@@ -1,13 +1,65 @@
-exports.check = function (message, by, room) {
-	if (![].includes(room)) return;;
-	by = by.substr(1);
-	if (/kicks.*snom/.test(message)) return Bot.say(room, '/me kicks ' + by);
-	if (toId(message) == 'kden' && toId(by) == 'joltofjustice') return Bot.say(room, 'Kden.');
-	if (/^\/me flee/.test(message)) return Bot.say(room, `/me catches ${by}`);
-	if (/^me runs/.test(message)) return Bot.say(room, `/me chases ${by} down`);
-	if (/^\/me hides/.test(message) && !message.toLowerCase().includes(Bot.status.nickName.toLowerCase())) return Bot.say(room, `/me places a big arrow above ${by}`);
-	if (/^right/.test(message) && toId(by) !== 'partman') return;
-	if (new RegExp(`, ?${Bot.status.nickName}\.?$`, 'i').test(message) && !/\. /.test(message)) return Bot.say(room, message.replace(new RegExp(`, ?${Bot.status.nickName}\.?$`, 'i'), '') + ', ' + by + '.');
-	if (new RegExp(`^/me .*${Bot.status.nickName}$`, 'i').test(message) && !/\bpart(?:man)?\b/i.test(message) && !message.match(new RegExp(Bot.status.nickName + '.+' + Bot.status.nickName, 'i'))) return Bot.say(room, message.replace(new RegExp(`${Bot.status.nickName}`, 'i'), by));
-	if (new RegExp(`^${Bot.status.nickName} is a `, 'i').test(message)) return Bot.say(room, 'N-no, you.');
+module.exports = function (room, time, by, message) {
+	if (typeof Bot.jpcool[room] === 'object') Object.keys(Bot.jpcool[room]).forEach(user => Bot.jpcool[room][user][0]++);
+	if (message.toLowerCase() === Bot.status.nickName.toLowerCase() + '?') return Bot.pm(by, `Hi, I'm ${Bot.status.nickName}! I'm a Bot by ${config.owner}. My prefix is \`\`${prefix}\`\`. For more information, use \`\`${prefix}help\`\`.`);
+	if (toId(message) === toId(Bot.status.nickName + ' for help')) return Bot.pm(by, '-_-');
+	if (!Bot.auth.admin.includes(toId(by)) && message.toLowerCase().includes(' punts snom')) return Bot.say(room, `/me punts ${by.substr(1)}`);
+	if (tools.hasPermission(by, 'none', room)) require('./autores.js').check(message, by, room);
+	if (!message.startsWith(prefix) || (ignoreRooms.includes(room) && !Bot.auth.admin.includes(toId(by)))) return;
+	if (message === `${prefix}eval 1`) return Bot.say(room, '1');
+	let args = message.substr(prefix.length).split(' ');
+	if (args[0].toLowerCase() == 'constructor') return Bot.say(room, `/me constructs a constrictor to constrict ${by.substr(1)}`);
+	const commandName = tools.commandAlias(args.shift().toLowerCase());
+	if (!commandName) return;
+	if (['eval','output'].includes(commandName)) {
+		if (!tools.hasPermission(toId(by), 'admin')) return Bot.pm(by, 'Access denied.');
+		try {
+			let outp = eval(args.join(' '));
+			switch (typeof outp) {
+				case 'object': outp = (commandName == 'eval') ? JSON.stringify(outp) : JSON.stringify(outp, null, 2); break;
+				case 'function': outp = outp.toString(); break;
+				case 'undefined': outp = 'undefined'; break;
+			}
+			if (commandName === 'eval' || (!String(outp).split('\n')[1] && String(outp.length > 300))) Bot.say(room, String(outp).replace(/hydrocity/gi,'REDACTED').replaceAll(config.pass, 'UwU'));
+			else Bot.say(room, '!code ' + String(outp).replace(/hydrocity/gi,'REDACTED').replaceAll(config.pass, 'UwU'));
+		} catch (e) {console.log(e); Bot.say(room, e.message)};
+		return;
+	}
+	const commandFile = commandName + '.js';
+	const commandObject = {};
+	new Promise((resolve, reject) => {
+		fs.readdir('./commands/global', (err, files) => {
+			if (err) reject(err);
+			commandObject.global = files;
+			resolve();
+		});
+	}).then(() => {
+		return new Promise((resolve, reject) => {
+			fs.readdir(`./commands/${room}`, (err, files) => {
+				if (err) {
+					commandObject[room] = [];
+					return resolve();
+				}
+				commandObject[room] = files;
+				resolve();
+			});
+		});
+	}).then(() => {
+		if (!commandObject[room].includes(commandFile) && !commandObject.global.includes(commandFile)) return Bot.pm(by, "Sorry, it doesn't look that command exists!");
+		let loc = room;
+		if (!commandObject[room].includes(commandFile)) loc = 'global';
+		const cReq = require(`./commands/${loc}/${commandFile}`);
+		if (!tools.hasPermission(by, cReq.permissions, room)) return Bot.pm(by, "Access denied.");
+		try {
+			if (cooldownObject[room] && cooldownObject[room][commandName]) return Bot.pm(by, "Sorry, cooling down.");
+			cReq.commandFunction(Bot, room, time, by, args, client);
+			tools.setCooldown(commandName, room, cReq);
+		} catch (err) {
+			Bot.pm(by, `Sorry, something went wrong! Error: ${err.message}`);
+			Bot.log(err);
+		}
+	}).catch(err => {
+		Bot.pm(by, `Sorry, something went wrong! Error: ${err.message}`);
+		Bot.log(err);
+	});
+	return;
 }
