f0b485097a64487d6c416a817c66a93c9dcfd2b8
inhibition <57114107+inhibition@users.noreply.github.com>
2020-10-11 23:12:36 +0530

    Update tools.js (#45)

--- data/tools.js
+++ data/tools.js
@@ -385,6 +385,43 @@ exports.colourize = function (text, name, useOriginal) {
 	return '<strong style=\"' + `color:hsl(${H},${S}%,${L}%);` + '\">' + text + '</strong>';
 }
 
+exports.HSLtoRGB = function (h, s, l) {
+	// input [[0, 360], [0, 100], [0, 100]]
+	let r, g, b;
+
+	h /= 360;
+	s /= 100;
+	l /= 100;
+
+	if (s == 0) r = g = b = l;
+	else {
+		let hue2rgb = (p, q, t) => {
+			if(t < 0) t += 1;
+			if(t > 1) t -= 1;
+			if(t < 1 / 6) return p + (q - p) * 6 * t;
+			if(t < 1 / 2) return q;
+			if(t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+			return p;
+		}
+
+		let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		let p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+
+	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+exports.RGBtoHEX = function (r, g, b) {
+	return ('#' + r.toString(16) + g.toString(16) + b.toString(16)).toUpperCase();
+}
+
+exports.HSLtoHEX = function (h, s, l) {
+	return exports.RGBtoHEX(...exports.HSLtoRGB(h, s, l));
+}
+
 exports.modeArray = function (arr) {
 	if (!Array.isArray(arr)) return;
 	let arrObj = {};
