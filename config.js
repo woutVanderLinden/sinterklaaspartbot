47b7f3350861d3d9ee096fa94ad477d3fcc5cae7
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-10-21 12:41:39 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,48 @@
+/* Configuration */
+
+exports.prefix = '';
+exports.nickName = '';
+exports.pass = '';
+exports.token = '';
+exports.owner = '';
+exports.site = true;
+exports.useDiscord = true;
+exports.webPort = 8080;
+exports.websiteLink = 'http://partbot.partman.co.in'; // You can also use an IP:port here
+
+
+/* Customization */
+
+exports.avatar = 'supernerd';
+exports.autoJoin = ['botdevelopment'];
+exports.status = false;
+exports.ignoreRooms = [];
+exports.tourTimerRooms = [];
+exports.activity = '';
+
+/* Global Authority */
+
+exports.auth = {
+	admin: [],
+	adminalts: [],
+	coder: [],
+	coderalts: [],
+	alpha: [],
+	alphaalts: [],
+	pseudoalpha: [],
+	beta: [],
+	betaalts: [],
+	pseudobeta: [],
+	gamma: [],
+	gammaalts: [],
+	pseudogamma: [],
+	locked: [],
+	lockedalts: []
+}
+
+
+/* Magic-y stuff, don't touch unless you know what you're doing */
+
+exports.server = 'sim.smogon.com';
+exports.port = 8000;
+exports.serverid = 'showdown';
