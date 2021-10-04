a2700d3b29e55609af16a72d239e819920fffc0b
PartMan <47669599+PartMan7@users.noreply.github.com>
2021-10-04 12:33:36 +0530

    Fix PoGO commands (#58)
    * Fix PoGO commands
    
    * Update Raid Help
    
    * Make pingraid lock the raid
    
    * Fix an oopsie
    
    * Add hostTimer clearing

--- commands/pokemongo/hostraid.js
+++ commands/pokemongo/hostraid.js
@@ -6,14 +6,16 @@ module.exports = {
 		const protoDB = require('origindb')('data/POGO'), DB = protoDB('users');
 		const DBU = DB.get(toID(by));
 		if (!DBU) return Bot.roomReply(room, by, 'Please register, first!');
-		args = args.join('').split(',').map(toID);
-		let mon = args.shift();
+		if (DBU.hostBanned || DBU.raidBanned) return Bot.roomReply(room, by, `You are banned from hosting raids - please contact a roomstaff member for further details.`);
+		args = args.join(' ').split(',');
+		let mon = toID(args.shift());
 		const weathers = {
 			"sunny": ["fire", "grass", "ground"],
 			"clear": ["fire", "grass", "ground"],
 			"partlycloudy": ["normal", "rock"],
 			"cloudy": ["fairy", "fighting", "poison"],
 			"rain": ["water", "electric", "bug"],
+			"rainy": ["water", "electric", "bug"],
 			"snow": ["ice", "steel"],
 			"fog": ["dark", "ghost"],
 			"windy": ["dragon", "flying", "psychic"],
@@ -39,42 +41,77 @@ module.exports = {
 			steel:    'https://static.wikia.nocookie.net/pokemongo/images/3/38/Icon_Steel.png',
 			water:    'https://static.wikia.nocookie.net/pokemongo/images/6/65/Icon_Water.png'
 		}
-		let weather = 'unknown', slots = 5;
-		let timeLeft;
-		args.forEach(arg => {
+		let weather = 'unknown', slots;
+		let timeLeft, timeHost, note;
+		if (args.find(fullArg => {
+			let arg = toID(fullArg);
 			let num = ~~arg.replace(/slots?/, '');
-			if (num) return slots = num;
-			if (weathers[arg]) return weather = arg;
+			if (num) {
+				if (slots) return Bot.roomReply(room, by, `Dunno why you're specifying the number of slots twice. Most of the time, this is because you forgot to mention a unit of time - if you instead meant ${num} minute(s) or something, then say so! It's the same thing as telling someone 'You have 10 to live'. 10 what? Days? Years? Seconds because you're trying to push me off a building?`) || true;
+				if (num > 19) return Bot.roomReply(room, by, `OIIIII YOU CAN ONLY HAVE 19 PEOPLE`) || true;
+				return (slots = num) && false;
+			}
+			arg = arg.replace(/weather/, '');
+			if (arg === 'constructor') return Bot.roomReply(room, by, `Clear'); DROP TABLE weathers;`) || true;
+			if (weathers[arg]) return (weather = arg) && false;
+			if (/^hostingin/.test(arg)) {
+				arg = toID(arg).substr('hostingin'.length);
+				const timeHostRun = tools.fromHumanTime(arg);
+				if (!timeHostRun) return Bot.roomReply(room, by, `Invalid value of hosting time: ${arg}`) || true;
+				if (timeHost) return Bot.roomReply(room, by, `Uhh why are you hosting at two times again\nIs this what they call two-timing?`) || true;
+				timeHost = timeHostRun;
+				return false;
+			}
+			if (arg.startsWith('note')) {
+				note = fullArg.replace(/note\W*/i, '');
+				if (note === fullArg) return Bot.roomReply(room, by, `C-could you please not add random characters in the note?`) || true;
+				return false;
+			}
 			const timeTry = tools.fromHumanTime(arg);
-			if (timeTry) return timeLeft = timeTry;
-			return Bot.roomReply(room, by, `Dunno what you meant by ${arg}, but I'll ignore it`);
-		});
+			if (timeTry) {
+				if (timeLeft) return Bot.roomReply(room, by, `...it's expiring twice? Are you sure you didn't mean to host at one of those instead? If so, add a 'hosting in' before that time!`) || true;
+				return (timeLeft = timeTry) && false;
+			}
+			return Bot.roomReply(room, by, `Dunno what you meant by ${arg}. Sorry, me smol brain 'o.o`) || true;
+		})) return;
+		if (!slots) slots = 5;
+		if (timeHost > timeLeft) return Bot.roomReply(room, by, `Isn't that, like, after the raid ends?`);
+		if (timeLeft > 4 * 60 * 60 * 1000) return Bot.roomReply(room, by, `Ah, yes. I, too, enjoy hosting raids that last for ${tools.toHumanTime(timeLeft)}`);
 		const user = toID(by);
 		if (!Bot.rooms[room].raids) Bot.rooms[room].raids = {};
-		if (Bot.rooms[room].raids[user]) return Bot.say(room, `/adduhtml RAIDHTML${by}, ${Bot.rooms[room].raids[user].HTML()}`);
+		if (Bot.rooms[room].raids[user]) return Bot.rooms[room].raids[user].HTML();
 		if (data.pokedex.hasOwnProperty(mon)) mon = data.pokedex[mon];
-		else return Bot.roomReply(room, by, 'Invalid Pokemon');
+		else return Bot.roomReply(room, by, 'Invalid Pokémon');
 		Bot.rooms[room].raids[user] = {};
 		const raid = Bot.rooms[room].raids[user];
 		raid.pokemon = mon.name;
 		raid.mon = toID(mon.name);
-		raid.wb = mon.types.some(t => weathers[weather].includes(toID(t)))
+		raid.weather = weather;
+		raid.wb = mon.types.some(t => weathers[raid.weather].includes(toID(t)));
 		raid.host = user;
-		raid.pings = (raid.wb ? Bot.rooms[room].users.filter(u => DB.get(toID(u))?.raids.hasOwnProperty(raid.mon)) : Bot.rooms[room].users.filter(u => DB.get(toID(u))?.raids.hasOwnProperty(raid.mon))).map(u => u.substr(1));
+		raid.pings = (raid.wb ? Bot.rooms[room].users.filter(u => DB.get(toID(u))?.raids.hasOwnProperty(raid.mon)) : Bot.rooms[room].users.filter(u => DB.get(toID(u))?.raids[raid.mon] === false)).map(u => u.substr(1));
 		raid.players = {};
 		raid.slots = slots;
 		raid.hostName = by.substr(1);
+		raid.note = note;
 		raid.HTML = function (pings) {
-			const players = Object.keys(raid.players).map(u => require('origindb')('data/POGO')('users').get(u));
-			let joinHTML = players.length ? `<br/><table style="border:1px;border-collapse:collapse;"><tr><th style="width:100px;">IGN</th><th style="width:30px;">Lv</th><th style="width:120px;">Friend Code</th></tr>${players.map(u => `<tr><td>${tools.escapeHTML(u.ign)}</td><td>${u.level}</td><td>${u.code}</td></tr>`).join('')}</table><br/>` : '-<br/><br/>';
-			if (players.length < slots && !raid.locked) joinHTML += `<button name="send" value="/botmsg ${Bot.status.nickName},${prefix}joinraid ${user}">Join</button>`;
+			const players = [raid.host, ...Object.keys(raid.players)].map(u => require('origindb')('data/POGO')('users').get(u));
+			let joinHTML = players.length > 1 ? `<br/><table style="border:1px;border-collapse:collapse;"><tr><th style="width:100px;">IGN</th><th style="width:30px;">Lv</th><th style="width:120px;">Friend Code</th></tr>${players.map(u => `<tr><td>${tools.escapeHTML(u.ign)}</td><td>${u.level}</td><td>${u.code.slice(0, 4)}<span style="padding:5px;">${u.code.slice(4, 8)}</span>${u.code.slice(8, 12)}</td></tr>`).join('')}</table><br/>` : '-<br/><br/>';
+			if (players.length <= slots && !raid.locked) joinHTML += `<button name="send" value="/botmsg ${Bot.status.nickName},${prefix}joinraid ${user}">Join</button>`;
 			joinHTML += `<button name="send" value="/botmsg ${Bot.status.nickName},${prefix}leaveraid ${user}">Leave</button>`;
-			let hiddenCodes = joinHTML.replace('<th style="width:120px;">Friend Code</th>', '').replace(/<td>\d+?<\/td>(?=<\/tr)/g, '');
-			if (pings) Bot.say(room, `/notifyrank all, Raid notification!, ${raid.pokemon}, ${raid.pings.join(' ')}`);
-			Bot.say(room, `/adduhtml RAIDHTML${by}, <div class="infobox" style="text-align:center;"><center><h2><psicon pokemon="${raid.mon}">${tools.colourize(raid.hostName)} is hosting a ${raid.wb ? 'Weather-Boosted ' : ''}<b>${raid.pokemon}</b> raid! <psicon pokemon="${raid.mon}"></h2>${weathers[weather].length ? `<small>Current boosted types: ${weathers[weather].map(type => `<img src="${icons[type]}" height="20" width="20" style="vertical-align:middle;"/>`).join(' ')}</small><br/><br/>` : ''}${hiddenCodes}${pings ? `<br/><br/><small>Pinged ${pings}</small>` : ''}</center></div>`);
-			Bot.say(room, `/changeprivateuhtml ${by}, RAIDHTML${by}, <div class="infobox" style="text-align:center;"><center><h2><psicon pokemon="${raid.mon}">${tools.colourize(raid.hostName)} is hosting a ${raid.wb ? 'Weather-Boosted ' : ''}<b>${raid.pokemon}</b> raid! <psicon pokemon="${raid.mon}"></h2>${weathers[weather].length ? `<small>Current boosted types: ${weathers[weather].map(type => `<img src="${icons[type]}" height="20" width="20" style="vertical-align:middle;"/>`).join(' ')}</small><br/><br/>` : ''}${joinHTML}${pings ? `<br/><br/><small>Pinged ${pings}</small>` : ''}</center></div>`);
+			let hiddenCodes = joinHTML.replace('<th style="width:120px;">Friend Code</th>', '').replace(/<td>\d{4}<span style="padding:5px;">\d{4}<\/span>\d{4}<\/td>(?=<\/tr)/g, '');
+			if (pings) raid.pings.forEach(u => Bot.say(room, `/notifyuser ${u}, Raid notification!, ${raid.pokemon}`));
+			Bot.say(room, `/adduhtml RAIDHTML${by}, <div class="infobox" style="text-align:center;"><center><h2><psicon pokemon="${raid.mon}">${tools.colourize(raid.hostName)} is hosting a ${raid.wb ? 'Weather-Boosted ' : ''}<b>${raid.pokemon}</b> raid! <psicon pokemon="${raid.mon}"></h2>${(weathers[raid.weather].length || raid.hostStamp) ? `<small>${weathers[raid.weather] ? `Current boosted types: ${weathers[raid.weather].map(type => `<img src="${icons[type]}" height="20" width="20" style="vertical-align:middle;"/>`).join(' ')}${raid.hostStamp ? '<br/>' : ''}` : ''}${raid.hostStamp || ''}</small><br/><br/>` : ''}${raid.note ? `Note: ${tools.escapeHTML(raid.note)}<br/>` : ''}${hiddenCodes}${pings ? `<br/><br/><small>Pinged ${pings}</small>` : ''}</center></div>`);
+			Bot.say(room, `/changeprivateuhtml ${by}, RAIDHTML${by}, <div class="infobox" style="text-align:center;"><center><h2><psicon pokemon="${raid.mon}">${tools.colourize(raid.hostName)} is hosting a ${raid.wb ? 'Weather-Boosted ' : ''}<b>${raid.pokemon}</b> raid! <psicon pokemon="${raid.mon}"></h2>${(weathers[raid.weather].length || raid.hostStamp) ? `<small>${weathers[raid.weather] ? `Current boosted types: ${weathers[raid.weather].map(type => `<img src="${icons[type]}" height="20" width="20" style="vertical-align:middle;"/>`).join(' ')}${raid.hostStamp ? '<br/>' : ''}` : ''}${raid.hostStamp || ''}</small><br/><br/>` : ''}${raid.note ? `Note: ${tools.escapeHTML(raid.note)}<br/>` : ''}${joinHTML}${pings ? `<br/><br/><small>Pinged ${pings}</small>` : ''}${players.length > 1 ? `<br/><br/><b style="border:1px;">${players.slice(1).map(u => tools.escapeHTML(u.ign)).join(',')}</b>` : ''}</center></div>`);
 		}
 		raid.endTime = Date.now() + (timeLeft || 100 * 60 * 1000);
+		if (timeHost) {
+			raid.hostTime = Date.now() + timeHost;
+			raid.hostStamp = `Hosting at <b>XX:${new Date(raid.hostTime).toLocaleTimeString('GMT', {timeZone: 'GMT'}).split(':')[1]}</b>`;
+			raid.hostTimer = setTimeout(() => {
+				Bot.say(room, `${Bot.rooms[room].raids[user].hostName}, you ready to host?`);
+			}, timeHost);
+		}
 		raid.timer = setTimeout(() => {
 			Bot.say(room, `${Bot.rooms[room].raids[user].hostName}'s raid expired!`);
 			delete Bot.rooms[room].raids[user];
@@ -83,7 +120,7 @@ module.exports = {
 			if (timeLeft > 10 * 60 * 1000) raid.warningTimer = setTimeout(() => Bot.say(room, `@${raid.hostName} there's, like, 10 minutes left; probably host soon - if you're done, use \`\`${prefix}endraid\`\`!`), timeLeft - 10 * 60 * 1000);
 			if (timeLeft > 5 * 60 * 1000) raid.fiveTimer = setTimeout(() => Bot.say(room, `@${raid.hostName} there's, like, 5 minutes left; people are likely to lose their passes if you raid any later. If you're done, use \`\`${prefix}endraid\`\`!`), timeLeft - 5 * 60 * 1000);
 		}
-		raid.hostFC = DBU.code;
+		raid.code = DBU.code;
 		const pings = tools.escapeHTML(tools.listify(raid.pings));
 		raid.HTML(pings);
 	}
