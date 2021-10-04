c6dd2420e07b6176efe3e6d80595c790461790cf
PartMan <47669599+PartMan7@users.noreply.github.com>
2021-10-04 12:32:14 +0530

    Make pingraid lock the raid

--- commands/pokemongo/pingraid.js
+++ commands/pokemongo/pingraid.js
@@ -7,11 +7,12 @@ module.exports = {
 		const raids = (Bot.rooms[room].raids || {});
 		if (!Object.keys(raids).length) return Bot.pm(by, "No raids are active, whoops");
 		const host = toID(args.join('')) || user;
-		if (!raids.hasOwnProperty(host)) return Bot.pm(by, `B-but who's hosting?`);
-		if (!tools.hasPermission(by, 'gamma', room) && host !== user) return Bot.pm(by, 'Access DENIED!');
+		if (!raids.hasOwnProperty(host)) return Bot.pm(by, `T-that user isn't hosting? I think?`);
+		if (!tools.hasPermission(by, 'gamma', room) && host !== user) return Bot.pm(by, 'Access DENIED - you may not ping for another user\'s raid!');
 		const raid = raids[host];
 		const pings = Object.values(raid.players);
 		if (!pings.length) return Bot.say(room, `B-but no one even joined D:`);
 		Bot.say(room, `/wall Raid time! Pinging ${tools.listify(pings)}`);
+		Bot.commandHandler('lockraid', by, args, room, true);
 	}
 }
